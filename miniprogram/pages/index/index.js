162885a08c92443d7552a292db6d70b3772e3b05
garfied <976680568@qq.com>
2019-09-18 00:44:59 +0800

    改动

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -17,22 +17,13 @@ Page({
       })
       return
     }
-
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
-            }
-          })
-        }
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {
+        params: ['0101', '0202']
       }
+    }).then((res) => {
+      console.log(res)
     })
   },
 
